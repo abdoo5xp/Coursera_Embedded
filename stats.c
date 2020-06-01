8788394edd77b8cc29df14091f6ce86b6ef17da8
abdoo5xp <abdooayman99@gmail.com>
2020-06-01 15:16:08 +0200

    Final Submission

--- stats.c
+++ stats.c
@@ -28,6 +28,7 @@
  */
 
 
+
 #include <stdio.h>
 #include "stats.h"
 
@@ -41,38 +42,113 @@ void main() {
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
-
+ 
+ print_statistics(test, SIZE);                 
 
 }
 
-void print_array(unsigned char *arry,char len){
+/* Add other Implementation File Code Here */
+
+void sort(unsigned char *arry,char len){
+	char flag;
+	do {
+		flag = 0;
+		for (char i = 0 ; i < len-1 ; i++)
+		{
+			if ( arry[i] < arry[i+1]){
+
+				swap(&arry[i] , &arry[i+1]);
+				flag = 1;
+			}
+
+		}
+
+	} while ( flag != 0 );
 
 }
 
 void swap(unsigned char *a, unsigned char *b){
+	unsigned char temp;
 
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
-void sort(unsigned char *arry,char len){
-
+void print_array(unsigned char *arry,char len){
+	
+	for(char i = 0;i < len; i++)
+	{
+		printf("\nElement number : %d = %d", i+1, arry[i]);
+	}
+	printf("\n");
 }
 
 unsigned char find_minimum(unsigned char *arry,char len){
 
+	unsigned char min;
+	min = arry[0];
+	char i = 0;
+
+	while(i < len){
+		if(arry[i] < min)
+			min = arry[i];
+		i++;
+	} 
+	return min;
 }
 
 unsigned char find_maximum(unsigned char *arry,char len){
-
+	
+	unsigned char max;
+	max = arry[0];
+	char i = 0;
+	
+	while(i < len){
+		if(arry[i] > max)
+			max = arry[i];
+		i++;
+	} 
+	return max;
 }
 
 unsigned char find_mean(unsigned char *arry,char len){
+	
+	unsigned char sum = 0;
 
+	for(char i = 0; i < len; i++)
+		sum += arry[i];
+	
+	return (sum/len);
 }
 
 unsigned char find_median(unsigned char *arry,char len){
 
+	unsigned char med;
+	char med_index ;
+	sort(arry,len);
+
+	if (len % 2 == 0){  		// number of elements is even.
+		med_index = len / 2;
+		med = (arry[med_index] + arry[med_index + 1]) / 2;
+	}
+	else { 						// number of elements is odd. 
+		med_index = len - 1 / 2;
+		med = arry[med_index];
+	}
+
+	return med;
 }
 
 void print_statistics(unsigned char *arry,char len){
-	
+	unsigned char min = find_minimum(arry, len);
+	unsigned char max = find_maximum(arry, len);
+	unsigned char median = find_median(arry, len);
+	unsigned char mean = find_mean(arry, len);
+
+	printf("Min:%d \n",min);
+	printf("Max:%d \n",max);
+	printf("Mean:%d \n",mean);
+	printf("Median:%d \n",median); 
+
 }
